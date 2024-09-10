0b6d8ee07f30160c9d2b39c9f4d039e144540a6a
Rahil Azhar <rahil.azhar10.com>
2024-09-10 11:48:14 +0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ...flowbite.content(),
+    flowbite.content(),
   ],
   darkMode: 'media', // Use 'media' to respect system theme
   theme: {
